8a4ae2fb218387c54770535fe37c19e3f54546ba
Adolf744x2 <den.denisovich.14@bk.ru>
2020-11-03 13:58:57 +1000

    Otecusholizcemyi

--- ConsoleApp1/Cat.cs
+++ ConsoleApp1/Cat.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -21,6 +24,65 @@ namespace ConsoleApp1
         {
             return (DateTime.Today - Birthday).Days / 365;
         }
-    }
+        byte _hungryStatus;
+        public byte HungryStatus
+        {
+            get { return _hungryStatus; }
+            set
+            {
+                if (value < 0)
+                {
+                    _hungryStatus = 0;
+                }
+                else if (value > 100)
+                {
+                    _hungryStatus = 100;
+                }
+                else
+                    _hungryStatus = value;
+            }
+        }
+        public void GetStatus()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст {GatAge()}");
+            if (HungryStatus <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кошка умирает от голода ");
+            }
+            else if (HungryStatus > 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Кошка очень голодна");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Кошка хочет кушать ");
+            }
+            else if (HungryStatus > 70 && HungryStatus <= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Кошка не против перекусить ");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Кошка недавно поела ");
+            }
+            Console.ResetColor();
+        }
+        async Task LifeCircle()
+
+        {
+            await Task.Delay(10000);
+            HungryStatus -= 10;
+            GetStatus();
+            await LifeCircle();
+            Task.Run(LifeCircle);
+        }
+
+}
 
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,11 @@ namespace ConsoleApp1
         {
             Cat vladik = new Cat(" Heroin", new DateTime(1998, 11, 02));
             vladik.Makenois();
+            vladik.HungryStatus = 150;
             Console.WriteLine($"Кошке по имени{vladik.Name} уже {vladik.GatAge()} лет");
+            Console.ReadLine();
+
         }
     }
+
 }
